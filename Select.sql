fa9e7ffae5e85ac8a48e3fad3c354534a3c14d17
serap-atay <75393492+serap-atay@users.noreply.github.com>
2022-04-10 01:26:16 +0300

    Inner join Examples

--- Select.sql
+++ Select.sql
@@ -65,3 +65,57 @@ SELECT TOP 10 ProductName,UnitPrice,UnitPrice*1.18 AS [Kdvli Fiyatı]
  INNER JOIN Suppliers S
  ON S.SupplierID=P.SupplierID
 
+ 
+ -- FEDERAL SHIPPING iLE TAŞINMIŞVE NANCY'NİN ALMIŞ OLDUĞU SİPARİŞLERİ GÖSTERİNİZ.
+
+ SELECT O.OrderID, E.FirstName,E.LastName,S.CompanyName FROM Orders O
+ INNER JOIN Employees E
+ ON O.EmployeeID= E.EmployeeID
+ INNER JOIN Shippers S
+ ON S.ShipperID=O.ShipVia
+ WHERE S.CompanyName='Federal Shipping' AND E.FirstName='Nancy'
+
+ -- MÜŞTERİ ADI,ONAYLAYAN ÇALIŞANIN ADI,ALDIĞI ÜRÜNLERİN ADINI LİSTELEYİN
+
+ SELECT C.ContactName,E.FirstName+' '+E.LastName AS EMPLOYEE, P.ProductName FROM 
+ Products P
+ INNER JOIN [Order Details] OD
+ ON P.ProductID=OD.ProductID
+ INNER JOIN Orders O
+ ON OD.OrderID=O.OrderID
+ INNER JOIN Employees E
+  ON E.EmployeeID=O.EmployeeID
+ INNER JOIN Customers C
+ ON C.CustomerID=O.CustomerID
+
+ -- AGGREGATE FUNC GROUP BY
+
+-- 01.01.1998 tarihinden sonra Siparis veren müşterilerin isimlerini ve siparis tarihlerini listeleyiniz
+
+SELECT C.ContactName,O.OrderDate
+FROM Orders O
+JOIN Customers C ON C.CustomerID = O.CustomerID
+WHERE O.OrderDate>'01.01.1998'
+
+-- 10248 nolu Sipariş hangi kargo sirketi ile gonderilmiştir.
+
+SELECT S.CompanyName
+FROM Orders O 
+JOIN Shippers S ON S.ShipperID = O.ShipVia
+WHERE O.OrderID = 10248
+
+-- TOFU isimli ürün alınan siparişlerin sipariş numaralarını listeleyiniz.
+
+SELECT OD.OrderID,P.ProductName FROM Products P
+INNER JOIN [Order Details] OD
+ON P.ProductID=OD.ProductID
+WHERE P.ProductName='TOFU'
+
+-- DUMON VEYA ALFKI MÜŞTERİLERİNİN ALDIĞI 1 ID Lİ ÇALIŞANIMIN ONAYLADIĞI 1 VEYA 3 NOLU KARGO FİRMASIYLA TAŞINMIŞ SİPARİŞLERİ GETİRİN
+
+SELECT *
+FROM Orders O 
+--WHERE (CustomerID='DUMON' OR CustomerID ='ALFKI') AND EmployeeID =1 AND (ShipVia = 1 OR ShipVia=3)
+WHERE CustomerID IN('DUMON','ALFKI') AND EmployeeID = 1 AND ShipVia IN(1,3)
+
+-- SUBQUERY
