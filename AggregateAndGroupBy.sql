0dbc87f045e86867817449cfc0efab59f0b02e3f
serap-atay <75393492+serap-atay@users.noreply.github.com>
2022-04-10 16:22:24 +0300

    Aggregate-Having-Distinct  examples

--- AggregateAndGroupBy.sql
+++ AggregateAndGroupBy.sql
@@ -75,3 +75,95 @@ SELECT P.ProductName,ROUND(SUM((1-OD.Discount)*OD.UnitPrice*OD.Quantity),2) AS T
  GROUP BY E.FirstName,E.LastName
  ORDER BY TUTAR DESC
 
+ -- Hangi üründen ne kadarlýk sipariþ edilmiþ(YILLARA GÖRE GRUPLAYINIZ)
+ SELECT P.ProductName,YEAR(O.OrderDate) AS YILLAR ,ROUND(SUM((1-OD.Discount)*OD.UnitPrice*OD.Quantity),2) AS TUTAR
+ FROM [Order Details] OD
+ INNER JOIN Orders O
+ ON O.OrderID=OD.OrderID
+ INNER JOIN Products P
+ ON P.ProductID=OD.ProductID
+ GROUP BY P.ProductName,YEAR(O.OrderDate)
+ ORDER BY P.ProductName ASC ,YEAR(O.OrderDate) DESC
+
+ -- Hangi üründen ne kadarlýk sipariþ edilmiþ(AYA GÖRE GRUPLAYINIZ)
+ SELECT P.ProductName,MONTH(O.OrderDate) AYLAR, ROUND(SUM((1-OD.Discount)*OD.UnitPrice*od.Quantity),2)
+ FROM [Order Details] OD
+ INNER JOIN Orders O
+ ON OD.OrderID=O.OrderID
+ INNER JOIN Products P
+ ON P.ProductID=OD.ProductID
+ GROUP BY P.ProductName,(O.OrderDate)
+
+-- Ortalama fiyatýn üstünden sattýðým ürünlerin fiyatýný getirin
+SELECT P.ProductName,P.UnitPrice
+FROM Products P
+WHERE P.UnitPrice > (
+				SELECT AVG(P.UnitPrice) AS ORTALAMA
+				FROM Products
+				)
+ORDER BY UnitPrice DESC
+
+-- Hangi müþterilerin ORTALAMA sipariþin üstünde sipariþ vermiþ ve ne kadarlýk sipariþ vermiþler
+
+SELECT C.CompanyName,SUM((1-OD.Discount)*OD.UnitPrice*OD.Quantity) TUTAR
+FROM Orders O 
+JOIN [Order Details] OD ON OD.OrderID = O.OrderID
+JOIN Customers C ON C.CustomerID = O.CustomerID
+WHERE (1-OD.Discount)*OD.UnitPrice*OD.Quantity>
+(
+	SELECT AVG((1-OD.Discount)*OD.UnitPrice*OD.Quantity) TUTAR
+	FROM [Order Details] OD
+)
+GROUP BY C.CompanyName
+ORDER BY TUTAR DESC
+
+--Adet olarak 
+SELECT TOP 5 C.CompanyName,COUNT(0) ADET
+FROM Orders O 
+JOIN Customers C ON C.CustomerID = O.CustomerID
+GROUP BY C.CompanyName
+HAVING COUNT(0) <
+(
+	SELECT AVG(SANAL.ADET) ORT
+	FROM 
+	(
+		SELECT O2.CustomerID,COUNT(0) ADET
+		FROM Orders O2
+		GROUP BY O2.CustomerID
+	) AS SANAL
+)
+ORDER BY ADET ASC
+
+-- Henüz ulaþmamýþ sipariþlerin toplam maaliyeti nedir?
+
+SELECT ROUND(SUM((1-OD.Discount)*OD.UnitPrice*OD.Quantity),2)
+FROM [Order Details] OD
+INNER JOIN Orders O
+ON O.OrderID=OD.OrderID
+WHERE O.ShippedDate IS NULL
+
+--Henüz ulaþmamýþ ürünler kaç gün beklemekte
+
+SELECT AVG(DATEDIFF(DAY,O.RequiredDate,GETDATE())) "Ortalama Bekleme"
+FROM Orders O
+WHERE O.ShippedDate IS NULL
+
+--Ortalama teslim zamanýmýz (gün)
+
+SELECT AVG(DATEDIFF(DAY,O.OrderDate,O.ShippedDate)) "Ortalama Bekleme"
+FROM Orders O
+WHERE O.ShippedDate IS NOT NULL
+
+--Sipariþ verilen ülkelerin sayýsý (Ayný ülke iki kez gösterilmeyecek)
+
+SELECT DISTINCT ShipCountry  
+FROM Orders
+
+--Henüz sipariþ vermemiþ müþteriler
+
+SELECT C.CompanyName,C.ContactName
+FROM Customers C
+WHERE C.CustomerID NOT IN(
+	SELECT DISTINCT O.CustomerID
+	FROM ORDERS O 
+)
